78cc647b0f141682f26bb7c1663e033b884971c8
Tom Hoover <tom.hoover.github@gmail.com>
2017-02-03 07:14:07 +0100

    Initial load of repository

---
+++ server.c
@@ -0,0 +1,35 @@
+
+
+//  Weather update server
+//  Binds PUB socket to tcp://*:5556
+//  Publishes random weather updates
+
+#include "zhelpers.h"
+
+int main (void)
+{
+    //  Prepare our context and publisher
+    void *context = zmq_ctx_new ();
+    void *publisher = zmq_socket (context, ZMQ_PUB);
+    //int rc = zmq_bind (publisher, "tcp://*:5556"); assert (rc == 0);
+    int rc = zmq_connect(publisher, "epgm://enp0s3;239.192.1.1:5555"); assert (rc == 0);
+
+
+    //  Initialize random number generator
+    srandom ((unsigned) time (NULL));
+    while (1) {
+        //  Get values that will fool the boss
+        int zipcode, temperature, relhumidity;
+        zipcode     = randof (100000);
+        temperature = randof (215) - 80;
+        relhumidity = randof (50) + 10;
+
+        //  Send message to all subscribers
+        char update [20];
+        sprintf (update, "%05d %d %d", zipcode, temperature, relhumidity);
+        s_send (publisher, update);
+    }
+    zmq_close (publisher);
+    zmq_ctx_destroy (context);
+    return 0;
+}
