b804cb51e0e423059010d7e4a664f0fe97043070
tom-hoover <tom.hoover@gmail.com>
2017-02-03 18:40:40 +0100

    Adding NORM support

--- server.c
+++ server.c
@@ -8,26 +8,30 @@
 
 int main (void)
 {
+
     //  Prepare our context and publisher
     void *context = zmq_ctx_new ();
     void *publisher = zmq_socket (context, ZMQ_PUB);
     //int rc = zmq_bind (publisher, "tcp://*:5556"); assert (rc == 0);
-    int rc = zmq_connect(publisher, "epgm://enp0s3;239.192.1.1:5555"); assert (rc == 0);
+    int hops = 128;
+    int rc = zmq_setsockopt( publisher, ZMQ_MULTICAST_HOPS, &hops, sizeof hops ); assert (rc==0);    
+    rc = zmq_connect(publisher, "norm://enp0s3;239.192.1.1:5555"); assert (rc == 0);
+
 
 
     //  Initialize random number generator
     srandom ((unsigned) time (NULL));
-    while (1) {
-        //  Get values that will fool the boss
-        int zipcode, temperature, relhumidity;
-        zipcode     = randof (100000);
-        temperature = randof (215) - 80;
-        relhumidity = randof (50) + 10;
+        int count = 0;    
+    while (count<100) {
+        printf( "count: %d\n", count );
+
 
         //  Send message to all subscribers
         char update [20];
-        sprintf (update, "%05d %d %d", zipcode, temperature, relhumidity);
+        sprintf (update, "%d", count++);
         s_send (publisher, update);
+        //sleep(1);
+
     }
     zmq_close (publisher);
     zmq_ctx_destroy (context);
